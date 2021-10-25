b89e5fd85f02743334b4c708c32f7624388dd599
weihua <1367856219@qq.com>
2021-10-25 18:55:41 +0800

    俩数之和

---
+++ data_structures/getting_started/day02/two_sum_test.go
@@ -0,0 +1,46 @@
+package day02
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test01",
+			args: args{nums: []int{2, 7, 11, 15}, target: 18},
+			want: []int{1, 2},
+		},
+		{
+			name: "test02",
+			args: args{nums: []int{3, 2, 4}, target: 5},
+			want: []int{0, 1},
+		},
+		{
+			name: "test03",
+			args: args{nums: []int{3, 3}, target: 6},
+			want: []int{0, 1},
+		},
+		{
+			name: "test04",
+			args: args{nums: []int{3, 2, 3}, target: 6},
+			want: []int{0, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
